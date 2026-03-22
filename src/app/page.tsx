176e94deb95f64eb37e4ca7b0e0153f24687a7b3
aman asati <asatiaman38@gmail.com>
2026-03-22 11:18:16 +0530

    05 ai jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,30 @@
 "use client"
 import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
 import { useTRPC } from "@/trpc/client"
 import { useMutation, useQuery } from "@tanstack/react-query"
+import { useState } from "react"
+import { toast } from "sonner"
 
 export default  function Home() {
+  const [value,setValue] = useState("");
  const trpc = useTRPC()
- const invoke = useMutation(trpc.invoke.mutationOptions({}))
- const data = useQuery(trpc.createAi.queryOptions({text : "Aman"}))
+ const invoke = useMutation(trpc.invoke.mutationOptions({
+  onSuccess : ()=>{
+    toast.success('Background job started');
+  }
+ }))
+//  const data = useQuery(trpc.createAi.queryOptions({text : "Aman"}))
   return (
     <>
-    <div className="w-7 h-3">
+    <div className="w-full h-screen">
+      <Input value={value} onChange={(e)=>setValue(e.target.value)}/>
       <Button onClick={()=>{
-        invoke.mutate({text:"aman"})
+        invoke.mutate({value: value})
       }}>
-        Invoke
+        Invoke BackGround job
       </Button>
-      <div>
-         {JSON.stringify(data.data)}
-      </div>
+      <div>{JSON.stringify(invoke.data)}</div>
     </div>
     </>
 )
